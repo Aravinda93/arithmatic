2204acbf18067f646120bcea28ffdab90c8b5876
Aravinda Baliga B <aravinda.baliga@benelog.com>
2022-07-08 10:38:11 +0200

    adding arithmatic project initial commit

---
+++ src/main/java/io/github/aravinda93/arithmatic/Operations.java
@@ -0,0 +1,22 @@
+package io.github.aravinda93.arithmatic;
+
+import java.math.BigDecimal;
+
+public class Operations {
+
+    public static BigDecimal add(final BigDecimal a, final BigDecimal b) {
+        return a.add(b);
+    }
+
+    public static BigDecimal subtract(final BigDecimal a, final BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    public static BigDecimal multiply(final BigDecimal a, final BigDecimal b) {
+        return a.multiply(b);
+    }
+
+    public static BigDecimal divide(final BigDecimal a, final BigDecimal b) {
+        return a.divide(b);
+    }
+}
